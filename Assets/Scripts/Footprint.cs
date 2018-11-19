11abea47bd511b4c033ff56acc2d6fe2c9bbc92a
Dean <dean3712@gmail.com>
2018-11-19 11:45:28 -0700

    Doctor's office mostly restored, and footprints added

--- Assets/Scripts/Footprint.cs
+++ Assets/Scripts/Footprint.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 //footprint prefab that is spawned, faded in, faded out, then destroys itself
 public class Footprint : MonoBehaviour {
 
-    public float m_TimeToLive = 1.5f;
+    public float m_TimeToLive = 3.0f;
     public AnimationCurve m_BrightnessCurve; //animation curve for fade in/out of footprint
 
-    private float m_TextureAlpha = 0;
+    private float m_StartingTime;
+    private float m_TimeOfDeath;
 	// Use this for initialization
 	void Start ()
     {
+        m_StartingTime = Time.time;
+        m_TimeOfDeath = Time.time + m_TimeToLive;
+
         m_BrightnessCurve.AddKey(0f, 0f);
         m_BrightnessCurve.AddKey(.1f, 1.0f);
         m_BrightnessCurve.AddKey(.7f, 1.0f);
@@ -21,10 +25,18 @@ public class Footprint : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        float time = Mathf.PingPong(Time.time, m_TimeToLive);
-        m_TextureAlpha = Map(time, 0, m_TimeToLive, 0, 1.0f);
+        float alphaTime = Map(Time.time, m_StartingTime, m_TimeOfDeath, 0, 1.0f);
+        Debug.Log("Alpha: " + alphaTime);
 
-        GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(1, 1, 1, m_BrightnessCurve.Evaluate(m_TextureAlpha)));
+        GetComponent<MeshRenderer>().material.SetColor("_Color", new Color(1, 1, 1, m_BrightnessCurve.Evaluate(alphaTime)));
+
+        if (Time.time - m_TimeOfDeath > 0)
+        {
+            //if time is past time of death
+            //kill destory footprint
+
+            Destroy(gameObject);
+        }
 	}
 
     private float Map(float value, float startMin, float startMax, float endMin, float endMax)
---
+++ Assets/Scripts/FootprintController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootprintController : MonoBehaviour {
+
+    public GameObject m_FootprintPrefab;
+    public Transform[] m_FootprintLocations;
+
+    public float m_StepInterval = 1.0f; //steps per second
+
+    private float m_StartingTime;
+    private bool m_IsPlayingFootsteps = false;
+    private int m_FootstepIndex = 0;
+	// Use this for initialization
+	void Start ()
+    {
+        m_StartingTime = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (m_IsPlayingFootsteps)
+        {
+            Debug.Log("Starting Footsteps");
+            if (Time.time - (m_StartingTime + m_StepInterval) >= 0)
+            {
+                //interval is complete
+                if (m_FootstepIndex >= m_FootprintLocations.Length)
+                {
+                    m_IsPlayingFootsteps = false;
+                    return;
+                }
+
+                PlaceNewFootstep(m_FootstepIndex);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            StartFootsteps();
+        }
+	}
+
+    private void PlaceNewFootstep(int locationIndex)
+    {
+        GameObject g = Instantiate(m_FootprintPrefab, m_FootprintLocations[locationIndex].position, Quaternion.identity, transform);
+        g.transform.localScale = m_FootprintLocations[locationIndex].localScale;
+        g.transform.localRotation = m_FootprintLocations[locationIndex].localRotation;
+
+        m_StartingTime = Time.time;
+
+        m_FootstepIndex++;
+    }
+
+    public void StartFootsteps()
+    {
+        m_IsPlayingFootsteps = true;
+        m_FootstepIndex = 0;
+
+        PlaceNewFootstep(0);
+    }
+}
