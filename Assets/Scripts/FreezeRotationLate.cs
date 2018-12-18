4978523e6abf4246f6931bdb1fd4c63436fa42ff
Dean <dean3712@gmail.com>
2018-12-18 12:37:33 -0700

    Basic prototype for wall text made

---
+++ Assets/Scripts/FreezeRotationLate.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeRotationLate : MonoBehaviour {
+
+    public bool m_FreezeXRotation;
+    public bool m_FreezeYRotation;
+    public bool m_FreezeZRotation;
+
+    private Quaternion m_StartingRotation;
+
+    private void Start()
+    {
+        m_StartingRotation = transform.rotation;
+    }
+    // Update is called once per frame
+    void LateUpdate ()
+    {
+        Vector3 setRotation = transform.rotation.eulerAngles;
+
+        if (m_FreezeXRotation) setRotation.x = m_StartingRotation.eulerAngles.x;
+        if (m_FreezeYRotation) setRotation.y = m_StartingRotation.eulerAngles.y;
+        if (m_FreezeZRotation) setRotation.z = m_StartingRotation.eulerAngles.z;
+
+        transform.rotation = Quaternion.Euler(setRotation);
+	}
+}
