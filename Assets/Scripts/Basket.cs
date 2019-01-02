f23281b34d4ce6531513a44c8895108027d8b5a6
Dean <dean3712@gmail.com>
2019-01-02 12:10:16 -0700

    Slowing down of player from non-painted objects

--- Assets/Scripts/Basket.cs
+++ Assets/Scripts/Basket.cs
@@ -1,17 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 /*
  * Hold/carry physics objects
  * Keep track of number of objects inside
+ * Specify how many of each item is required
+ * Slows player down if objects are not painted over
 */
 
-public class Basket : MonoBehaviour {
+public class Basket : MonoBehaviour
+{
+    public UnityStandardAssets.Characters.FirstPerson.FirstPersonController m_PlayerController;
+
+    public int m_NumItem1;
+    public int m_NumItem2;
+    public int m_NumItem3;
 
     private List<GroceryItem> m_BasketItems;
-	// Use this for initialization
+    // Use this for initialization
+
+    private void Start()
+    {
+        m_BasketItems = new List<GroceryItem>();
+    }
+
+    private void Update()
+    {
+        UpdatePlayerSpeed(); //TODO move this to an event system so it doesn't have to check every frame
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -36,4 +53,30 @@ public class Basket : MonoBehaviour {
             m_BasketItems.Remove(item);
         }
     }
+
+    private void UpdatePlayerSpeed()
+    {
+        int maxItems = m_NumItem1 + m_NumItem2 + m_NumItem3;
+        int numNonPaintedItems = GetNumItemsNotPainted();
+
+        float walkScalar = (float)numNonPaintedItems / (float)maxItems; //get percentage of items non painted
+        walkScalar = 1.0f - walkScalar; //inverse percentage to make more non painted items cause a slower walk speed
+
+        m_PlayerController.SetWalkScalar(walkScalar);
+    }
+
+    //goes through current basket items and gets number of items not painted over
+    private int GetNumItemsNotPainted()
+    {
+        int count = 0;
+        foreach (GroceryItem item in m_BasketItems)
+        {
+            if (!item.IsItemPainted())
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
 }
--- Assets/Scripts/GroceryItem.cs
+++ Assets/Scripts/GroceryItem.cs
@@ -8,9 +8,16 @@ public class GroceryItem : MonoBehaviour
 
     [SerializeField]
     private ItemType m_ItemType; //keeps this proctected from being set by other classes, only set in editor
+    [SerializeField]
+    private bool m_IsPaintedOver = true;
 
     public ItemType GetItemType()
     {
         return m_ItemType;
+    }
+
+    public bool IsItemPainted()
+    {
+        return m_IsPaintedOver;
     }
 }
