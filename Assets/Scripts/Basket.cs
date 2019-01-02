0e4b43e412611cf16efdd329c05a445c467b208d
Dean <dean3712@gmail.com>
2019-01-02 10:40:54 -0700

    Basket can hold objects and keep track of them

---
+++ Assets/Scripts/Basket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/*
+ * Hold/carry physics objects
+ * Keep track of number of objects inside
+*/
+
+public class Basket : MonoBehaviour {
+
+    private List<GroceryItem> m_BasketItems;
+	// Use this for initialization
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GroceryItem item = other.gameObject.GetComponent<GroceryItem>();
+
+        //if triggered object is a grocery item and is already not in basket list
+        if (item && !m_BasketItems.Contains(item))
+        {
+            //add item to basket list
+            m_BasketItems.Add(item);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        GroceryItem item = other.gameObject.GetComponent<GroceryItem>();
+
+        //if triggered object is a grocery item and is in basket list
+        if (item && m_BasketItems.Contains(item))
+        {
+            //remove item from basket list
+            m_BasketItems.Remove(item);
+        }
+    }
+}
---
+++ Assets/Scripts/GroceryItem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroceryItem : MonoBehaviour
+{
+    public enum ItemType {Love, Sphere, Can}
+
+    [SerializeField]
+    private ItemType m_ItemType; //keeps this proctected from being set by other classes, only set in editor
+
+    public ItemType GetItemType()
+    {
+        return m_ItemType;
+    }
+}
