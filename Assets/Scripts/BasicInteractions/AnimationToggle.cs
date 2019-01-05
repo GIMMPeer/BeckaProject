96709c6a7de27d00a0b5de414bc809e67a619ff6
Dean <dean3712@gmail.com>
2019-01-04 17:14:48 -0700

    Added simple animation toggle to grocery store freezer

---
+++ Assets/Scripts/BasicInteractions/AnimationToggle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationToggle : MonoBehaviour {
+
+    public string m_TriggerName = "Toggle";
+
+    private Animator m_Animator;
+    // Use this for initialization
+
+    private void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+    }
+
+    public void ToggleAnimation()
+    {
+        m_Animator.SetTrigger(m_TriggerName);
+    }
+}
