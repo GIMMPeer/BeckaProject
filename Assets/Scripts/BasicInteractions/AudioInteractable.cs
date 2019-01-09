9117e43889629ac4938062b1e39b9ba1ff64bce8
Dean <dean3712@gmail.com>
2019-01-09 13:54:06 -0700

    Added distortion shader for audio interactable

--- Assets/Scripts/BasicInteractions/AudioInteractable.cs
+++ Assets/Scripts/BasicInteractions/AudioInteractable.cs
@@ -34,6 +34,7 @@ public class AudioInteractable : MonoBehaviour {
             if (Time.time - m_AnimationStartTime >= 3f)
             {
                 Destroy(m_CurrentDistortionSphere);
+                Debug.Log(m_CurrentDistortionSphere);
                 m_CurrentDistortionSphere = null;
                 m_SphereIsAnimating = false;
             }
