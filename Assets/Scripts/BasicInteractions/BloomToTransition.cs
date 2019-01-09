d0236564c1672e16a0f740b0fd4789c4eaff8619
Dean <dean3712@gmail.com>
2019-01-09 11:16:28 -0700

    Doctor office transition works with bloom

--- Assets/Scripts/BasicInteractions/BloomToTransition.cs
+++ Assets/Scripts/BasicInteractions/BloomToTransition.cs
@@ -8,10 +8,13 @@ public class BloomToTransition : MonoBehaviour {
 
     public UnityEvent m_OnBloomComplete;
     public PostProcessingProfile m_PP;
+    public AnimationCurve m_LightCurve; //curve which defines the rate of change for bloom
 
     public float m_Duration = 2.0f;
     public float m_IntensityGoal = 100.0f;
     public float m_ThresholdGoal = 0f;
+
+    public bool m_InverseBloom = false; //make bloom start bright and end normal
 
     private BloomModel.Settings m_BloomSettings;
 
@@ -31,17 +34,21 @@ public class BloomToTransition : MonoBehaviour {
         if (m_IsLerping)
         {
             float timeDifference = Time.time - m_TimeStarted;
-            float lerpVal = Map(timeDifference, 0, m_Duration, 0f, 1f); //map time difference to lerp scale
+            float mappedTime = Map(timeDifference, 0, m_Duration, 0f, 1f); //map time difference to lerp scale
 
-            float intensityVal = Mathf.Lerp(m_PreviousIntensity, m_IntensityGoal, lerpVal); //lerp is clamped so it can never go above goal
-            float thresholdVal = Mathf.Lerp(m_PreviousIntensity, m_ThresholdGoal, lerpVal);
+            float inversedTime = m_InverseBloom ? 1.0f - mappedTime : mappedTime; //if inverse then flip value
+
+            float curveTime = m_LightCurve.Evaluate(inversedTime);
+
+            float intensityVal = Map(curveTime, 0.0f, 1.0f, m_PreviousIntensity, m_IntensityGoal);
+            float thresholdVal = Map(curveTime, 0.0f, 1.0f, m_PreviousThreshold, m_ThresholdGoal);
 
             m_BloomSettings.bloom.intensity = intensityVal;
             m_BloomSettings.bloom.threshold = thresholdVal;
 
             m_PP.bloom.settings = m_BloomSettings; //apply bloom settings to profile
 
-            if (lerpVal >= 0.99999f)
+            if (mappedTime >= 0.99999f)
             {
                 //transition is done
                 EndTransition();
