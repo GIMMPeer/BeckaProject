6d0442e92166321dd06b69236da7e597230c5514
Jarl_Alrerkr (Gabe) <gabegrow@u.boisestate.edu>
2019-01-16 11:33:19 -0700

    Added G-Bathroom and ChatterDictionary to Bathroom
    Edited ChatterDictionary.cs to include a multiplier for the text distance. 
    Added G-Bathroom. 
    Added Chatter Text to G-Bathroom. 
    
    Bug: Chatter text clips through other chatter text that has spawned in the same location as the box collider. 
    Chatter text does not rotate when moving parent gameobject. See Ln 27 ChatterDictionary.cs

--- Assets/Scripts/ChatterDictionary.cs
+++ Assets/Scripts/ChatterDictionary.cs
@@ -6,6 +6,7 @@ public class ChatterDictionary : MonoBehaviour {
 
     public Texture2D[] m_AllWords;
     public GameObject m_WordPrefab;
+    public float m_TextDistanceMultiplier;
 
     public int m_TotalWordCount = 1;
 	// Use this for initialization
@@ -14,7 +15,8 @@ public class ChatterDictionary : MonoBehaviour {
 		for (int i = 0; i < m_TotalWordCount; i++)
         {
             //offset is based on rotation of holder, always through local forward and up vectors
-            Vector3 offset = (transform.forward * Random.Range(-0.75f, 0.75f)) + (transform.up * Random.Range(-0.5f, 0.5f));
+            //Vector3 offset = (transform.forward * Random.Range(-0.75f, 0.75f)) + (transform.up * Random.Range(-0.5f, 0.5f));
+            Vector3 offset = (transform.forward * Random.Range(-m_WordPrefab.transform.localScale.x, m_WordPrefab.transform.localScale.x) * m_TextDistanceMultiplier) + (transform.up * Random.Range(-m_WordPrefab.transform.localScale.y, m_WordPrefab.transform.localScale.y)) * m_TextDistanceMultiplier * 1.2f;
 
             GameObject word = Instantiate(m_WordPrefab);
             word.transform.parent = transform;
