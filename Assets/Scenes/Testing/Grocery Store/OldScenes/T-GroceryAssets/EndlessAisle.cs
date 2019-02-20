db481d837367615348d101a18c82247575821c31
Dean <dean3712@gmail.com>
2019-02-20 13:19:01 -0700

    Reorganized scenes and fixed endless aisle

--- Assets/Scenes/Testing/Grocery Store/OldScenes/T-GroceryAssets/EndlessAisle.cs
+++ Assets/Scenes/Testing/Grocery Store/OldScenes/T-GroceryAssets/EndlessAisle.cs
@@ -2,42 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EndlessAisle : MonoBehaviour {
-	public GameObject player; //set the player
-	private Transform playerLocation; //idenitfy the player's location in game
-	private float startZ; //player start depth
-	private float endZ; // player end depth
-	private float startTeleTarget; //start location player teleportation trigger target depth
-	private float endTeleTarget; //end location player teleportation target trigger depth
-	private Vector3 resetStartLocation; //used to reset player location
+//TODO change name from Z to X
+public class EndlessAisle : MonoBehaviour
+{
 
-	void Start () {
-		//assignments
-		startZ = player.transform.position.z;
-		startTeleTarget = 34.5f;
-		resetStartLocation.Set(0, 0, 0);
-		endZ = -20.0f;
-		endTeleTarget = -20.5f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		playerLocation = player.transform; //updating player location in realtime
-		
-		if (playerLocation.position.z < endTeleTarget) //the value we are evaluating against may need to be adjusted based on level design
-		{
-			//we only want to reset Z depth so we adapt to player's current position for X and Y
-			resetStartLocation.x = playerLocation.position.x;
-			resetStartLocation.y = playerLocation.position.y;
-			resetStartLocation.z = startZ;
-			player.transform.position = resetStartLocation; //Move the player to start
-		} 
-		else if (playerLocation.position.z > startTeleTarget) 
-		{
-			resetStartLocation.x = playerLocation.position.x;
-			resetStartLocation.y = playerLocation.position.y;
-			resetStartLocation.z = endZ;
-			player.transform.position = resetStartLocation; //Move the player to end
-		}
-	}
+    public GameObject player; //set the player
+    private Transform playerLocation; //idenitfy the player's location in game
+    private float startZ; //player start depth
+    private float endZ; // player end depth
+    private float startTeleTarget; //start location player teleportation trigger target depth
+    private float endTeleTarget; //end location player teleportation target trigger depth
+    private Vector3 resetStartLocation; //used to reset player location
+
+    void Start()
+    {
+        //assignments
+        startZ = 20;
+        startTeleTarget = 21.0f; //in world space
+        resetStartLocation.Set(0, 0, 0);
+        endZ = 16.2f;
+        endTeleTarget = 15.9f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerLocation = player.transform; //updating player location in realtime
+
+        if (playerLocation.position.x < endTeleTarget) //the value we are evaluating against may need to be adjusted based on level design
+        {
+            //we only want to reset Z depth so we adapt to player's current position for X and Y
+            resetStartLocation.x = startZ;
+            resetStartLocation.y = playerLocation.position.y;
+            resetStartLocation.z = playerLocation.position.z;
+            player.transform.position = resetStartLocation; //Move the player to start
+        }
+        else if (playerLocation.position.x > startTeleTarget)
+        {
+            resetStartLocation.x = endZ;
+            resetStartLocation.y = playerLocation.position.y;
+            resetStartLocation.z = playerLocation.position.z;
+            player.transform.position = resetStartLocation; //Move the player to end
+        }
+    }
+
+    public void SetEndValue(float val)
+    {
+        endTeleTarget = val;
+        endZ = val + 0.5f;
+    }
 }
