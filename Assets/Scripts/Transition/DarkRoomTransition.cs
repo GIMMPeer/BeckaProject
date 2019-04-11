8389a96f5f94e9baf80d7e82a313a19ed3009dd6
Dean <dean3712@gmail.com>
2019-04-11 14:35:13 -0600

    Transition room works with transition and new movement

--- Assets/Scripts/Transition/DarkRoomTransition.cs
+++ Assets/Scripts/Transition/DarkRoomTransition.cs
@@ -20,13 +20,16 @@ public class DarkRoomTransition : MonoBehaviour {
 
         Vector3 playerDirection = m_Player.position - m_Door.position;
 
-        float distanceDot = Vector3.Dot(playerDirection, m_Door.right); //door's right vector is door's forwards facing side, check to make sure player is on the correct side of the door
-        float playerViewDot = Vector3.Dot(Camera.main.transform.forward, m_Door.right);
+        float distanceDot = Vector3.Dot(playerDirection.normalized, Vector3.right); //right vector is door's forwards facing side, check to make sure player is on the correct side of the door
+        float playerViewDot = Vector3.Dot(Camera.main.transform.forward, Vector3.right);
 
-        if (distance >= m_TriggerDistance)
+
+        if (distance <= m_TriggerDistance)
         {
             if (distanceDot <= 0.1f) //if player has crossed threshold of door
             {
+                Debug.Log("Dot: " + playerViewDot);
+
                 if (playerViewDot <= -0.4f) //if player is not looking at door
                 {
                     if (m_PlayerInsideDarkBox)
